2a840e923637e273d3f491956f64422888b4f595
Ebou Jobe <ebou.jobe@mail.mcgill.ca>
2018-03-16 15:27:37 -0400

    refactored the code to improve efficiecy

--- src/ca/mcgill/ecse211/tests/BlockScanner.java
+++ src/ca/mcgill/ecse211/tests/BlockScanner.java
@@ -35,40 +35,45 @@ public class BlockScanner extends Thread {
 		// put all the distances , and angles measured in the hashMap
 		
 		// spin 90 clockwise
-		long endTimeMillis = System.currentTimeMillis() + 4000;
+		long endTimeMillis = System.currentTimeMillis() + 2900;
 	    while (true) {
 	        nav.spin(true);
-	        map.put(us.getDistance(), odo.getXYT()[2]);
-	        System.out.println(map);
+	        float temp= us.getDistance();
+	        map.put(temp, odo.getXYT()[2]);
+	        smallestDistance = temp < smallestDistance ?  temp  : smallestDistance;
 	        if (System.currentTimeMillis() > endTimeMillis) {
 	            break;
 	        }
 	    }
-	    
-	 // spin 90 anti-clockwise
-	    
-		long endTimeMillisCW = System.currentTimeMillis() + 4000;
+		long endTimeMillisCW = System.currentTimeMillis() + 2900;
 	    while (true) {
 	        nav.spin(false);
-	        map.put(us.getDistance(), odo.getXYT()[2]);
-	        System.out.println(map);
+	        float temp= us.getDistance();
+	        map.put(temp, odo.getXYT()[2]);
+	        smallestDistance = temp < smallestDistance ?  temp  : smallestDistance;
 	        if (System.currentTimeMillis() > endTimeMillisCW) {
 	            break;
 	        }
 	    }
 	    
+	 // spin 90 anti-clockwise
+	    	    
 	    // loop through the hashMap and get the smallest distance and the smallest angle
 
 	    double smallestAngle = Double.MAX_VALUE;
-	    
-	    for(float distance: map.keySet()) {
-	       if(distance < smallestDistance){
-	    	   		smallestDistance = distance;
-	       }
-	    }
+	 // inefficient find a solution for it   
+//	    for(float distance: map.keySet()) {
+//	       if(distance < smallestDistance && distance != 0){
+//	    	   		smallestDistance = distance;
+//	       }
+//	    }
 	    smallestAngle = map.get(smallestDistance);
-         if(smallestDistance < 30f) {
+	    System.out.println("smallest distance is " + smallestDistance);
+	    System.out.println("Smallest angle is " + smallestAngle );
+         if(smallestDistance < 60f) {
+        	 	Sound.beep();
         	 	nav.turnTo(smallestAngle);
+        	 	nav.move(this.smallestDistance-4, false);
         	 	return true;
          }
     
